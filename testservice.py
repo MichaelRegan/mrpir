d657e0e77ac06cbe80243023ea8f1cc6ee74a797
Michael Regan <Michael.Regan.Sr@gmail.com>
2023-06-09 09:56:24 -0700

    test implementation working

--- testservice.py
+++ testservice.py
@@ -17,22 +17,22 @@ if __name__ == '__main__':
 class Pirservice:
 
     # Private variables
-    _systemd_notify = None
+    _systemd_notify = sdnotify.SystemdNotifier()
+    _logger = logging.getLogger(__name__)
     _mqtt_client = None
-    _logger = None
+
     _mqtt_connection_error = False
     _mqtt_connection_error_rc = 0
-    
     shutdown_requested = False
     
     def __init__(self):
         signal.signal(signal.SIGINT, self.request_shutdown)
         signal.signal(signal.SIGTERM, self.request_shutdown)
-        _logger = logging.getLogger('mrpir')
+        # _logger = logging.getLogger(__name__)
         
-        time.sleep(1)
+        # time.sleep(1)
         # Tell systemd that our service is ready
-        Pirservice._systemd_notify = sdnotify.SystemdNotifier()
+        # Pirservice._systemd_notify = sdnotify.SystemdNotifier()
 
         Pirservice._systemd_notify.notify("Status=Setting up logging")
         # Setup logging
@@ -65,24 +65,40 @@ class Pirservice:
         self.MQTT_PORT = config ("MQTT_PORT", default=1883, cast=int)
         self.PIR_PIN = config ("PIR_PIN", default=23)
         self.LOGGING_LEVEL = config ("LOGGING_LEVEL", default=1, cast=int) * 10
-        _logger.setLevel(self.LOGGING_LEVEL)
-        _logger.info(str(_logger.getEffectiveLevel))
+        Pirservice._logger.setLevel(self.LOGGING_LEVEL)
+        Pirservice._logger.info(str(Pirservice._logger.getEffectiveLevel))
 
-        Pirservice._systemd_notify.notify("Status=Setting up MQTT Client")
         # Setup MQTT
-        Pirservice._mqtt_client = mqtt.Client(self.MQTT_CLIENT_ID)
-        Pirservice._mqtt_client.enable_logger()
+        Pirservice._systemd_notify.notify("Status=Setting up MQTT Client")
+        Pirservice._mqtt_client = mqtt.Client(self.MQTT_CLIENT_ID, clean_session=True, userdata=None, protocol=mqtt.MQTTv311, transport="tcp")
+        Pirservice._mqtt_client.enable_logger(Pirservice._logger)
         Pirservice._mqtt_client.on_connect = self.on_connect
         Pirservice._mqtt_client.on_disconnect = self.on_disconnect
         Pirservice._mqtt_client.on_log = self.on_log
         Pirservice._mqtt_client.username_pw_set(self.MQTT_USER, self.MQTT_PASSWORD)
-        
+        self.is_mqtt_connected = False
+
+        # Connect to the MQTT broker and start the background thread
+        Pirservice._mqtt_client.connect(self.MQTT_BROKER, self.MQTT_PORT, keepalive=45)
+        Pirservice._mqtt_client.loop_start()
+
+
+        Pirservice._systemd_notify.notify("Status=Setting up PIR connection")
+        self.pir = MotionSensor(self.PIR_PIN)
 
         # Finally, notify systemd
         Pirservice._systemd_notify.notify("READY=1")
 
     def on_connect(self, client, userdata, flags, rc):
+        # 0: Connection successful 
+        # 1: Connection refused - incorrect protocol version 
+        # 2: Connection refused - invalid client identifier 
+        # 3: Connection refused - server unavailable 
+        # 4: Connection refused - bad username or password 
+        # 5: Connection refused - not authorised 
+        # 6-255: Currently unused.
         if rc == 0:
+            self.is_mqtt_connected = True     
             self._logger.info("Connected to MQTT broker")
             self._mqtt_client.subscribe(self.TOPIC)
             self._mqtt_client.publish(self.CONFIG_TOPIC, self.CONFIG_PAYLOAD, retain=True)
@@ -92,13 +108,17 @@ class Pirservice:
     def on_disconnect(self, client, userdata, rc): # pylint: disable=unused-argument
         if rc != 0:
             self._logger.warning("Unexpected disconnection from MQTT broker")
+            self.is_mqtt_connected = False
 
     def on_log(self, client, userdata, level, buf): # pylint: disable=unused-argument # pylint: disable=invalid-name 
         self._logger.debug("MQTT log: %s", buf)
 
     def __del__(self):
-        shutdown_requested = False
-        # pir.close()
+        self.pir.close()
+
+        # Start a background thread to process MQTT messages
+        Pirservice._mqtt_client.loop_stop()
+
 
     def can_run(self):
         """ can run for SIGINT """
@@ -111,51 +131,31 @@ class Pirservice:
         self.shutdown_requested = True
         Pirservice._systemd_notify.notify("STOPPING=1")
 
-    def connect_mqtt(self):
-        """ Connect to the MQTT broker and setup callbacks """
-        # Set Connecting Client ID
-        Pirservice._mqtt_client.connect(self.MQTT_BROKER, self.MQTT_PORT, keepalive=45)
-        # self._mqtt_client.connect(self.MQTT_BROKER, self.MQTT_PORT, keepalive=45)
+def on_motion():
+    Pirservice._logger.info("Motion detected")
+    # pirservice.connect_mqtt()
+    Pirservice._mqtt_client.publish(Pirservice.TOPIC, "ON", retain=True)
 
-        self._mqtt_client.enable_logger()
-        self._mqtt_client.loop_start()
-
-        # Give some time for the connection to be established
-        while not self._mqtt_client.is_connected() and not self._mqtt_connection_error:
-            time.sleep(1)
-
-        self._mqtt_client.loop_stop()
-        if self._mqtt_connection_error:
-            raise ConnectionError('Connection Error', self._mqtt_connection_error_rc)
-
-        return self._mqtt_client
-
-
-    def on_motion():
-        Pirservice._logger.info("Motion detected")
-        pirservice.connect_mqtt()
-        Pirservice._mqtt_client.publish(Pirservice.TOPIC, "ON", retain=True)
-
-    def on_no_motion():
-        Pirservice._logger.info("No motion detected")
-        pirservice.connect_mqtt()
-        Pirservice._mqtt_client.publish(Pirservice.TOPIC, "OFF", retain=True)
+def on_no_motion():
+    Pirservice._logger.info("No motion detected")
+    pirservice.connect_mqtt()
+    Pirservice._mqtt_client.publish(Pirservice.TOPIC, "OFF", retain=True)
 
 pirservice = Pirservice()
 Pirservice._systemd_notify.notify("Status=entering main loop")
 # Pirservice._logger.info('entering main loop')
 
 try:
-    pir = MotionSensor(pirservice.PIR_PIN)
-    pir.when_motion = pirservice.on_motion
-    pir.when_no_motion = pirservice.on_no_motion
+
+    pirservice.when_motion = on_motion
+    pirservice.when_no_motion = on_no_motion
 
     while pirservice.can_run():
-        # blocking... should use wait_for_motion(timeout=5)
-        pir.wait_for_motion(timeout=5)
+        # mqtt is working on a background thread, so we need to wait for it to connect
+        pirservice.pir.wait_for_motion(timeout=5)
         Pirservice._systemd_notify.notify("WATCHDOG=1")
-        pir.wait_for_no_motion(timeout=15)
+        pirservice.pir.wait_for_no_motion(timeout=5)
         Pirservice._systemd_notify.notify("WATCHDOG=1")
 
 finally:
-    pir.close()
+    pirservice.pir.close()
