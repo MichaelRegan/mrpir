801f8c4bc94010e54ea9aef445de780ee5c8cb8f
Michael Regan <michael.regan.sr@gmail.com>
2024-09-02 10:19:09 -0700

    Move log file to ~/.mrpir/logs

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -8,8 +8,7 @@ import logging
 import logging.config
 import yaml  # pylint: disable=import-error
 
-
-def setup_logging(default_path: str = '../config/log_config.yaml',
+def setup_logging(default_path: str = '~/.mrpir/config/log_config.yaml',
                   default_level: int = logging.INFO) -> None:
     """
     Sets up logging configuration from a YAML file. If the file is not found or
@@ -20,15 +19,25 @@ def setup_logging(default_path: str = '../config/log_config.yaml',
         default_level (int): The default logging level to use if the configuration 
         file is not found or invalid.
     """
-    # Adjust the path relative to the script's location
-    base_path = os.path.dirname(os.path.dirname(__file__))  # Go up one directory from utils
-    path = os.path.join(base_path, default_path)
+    # Define the log directory within the user's home directory
+    log_dir = os.path.expanduser('~/.mrpir/logs')
 
-    if os.path.exists(path):
-        with open(path, 'rt', encoding='utf-8') as file:
+    # Ensure the log directory exists
+    os.makedirs(log_dir, exist_ok=True)
+
+    # Adjust the path to the logging configuration file, expanding user directory
+    config_path = os.path.expanduser(default_path)
+
+    if os.path.exists(config_path):
+        with open(config_path, 'rt', encoding='utf-8') as file:
             try:
                 config = yaml.safe_load(file.read())
                 if config is not None:
+                    # Dynamically update the logfile path in the configuration
+                    for handler in config.get('handlers', {}).values():
+                        if 'filename' in handler:
+                            handler['filename'] = os.path.join(log_dir, os.path.basename(handler['filename']))
+                    
                     logging.config.dictConfig(config)
                 else:
                     raise ValueError("Loaded YAML configuration is empty.")
@@ -36,9 +45,8 @@ def setup_logging(default_path: str = '../config/log_config.yaml',
                 print(f"Error parsing YAML file: {err}")
                 logging.basicConfig(level=default_level)
     else:
-        print(f"Logging configuration file not found: {path}")
+        print(f"Logging configuration file not found: {config_path}")
         logging.basicConfig(level=default_level)
-
 
 # Initialize the logger and set up logging configuration
 logger = logging.getLogger(__name__)
